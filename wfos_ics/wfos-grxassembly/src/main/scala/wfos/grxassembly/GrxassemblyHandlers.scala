f10f8ac7c79809c08d57064565d7cd6dda40a3ff
karthikreddyarem <karthikavreddy@gmail.com>
2022-03-31 20:01:26 +0530

    Validation method for GrxAssembly

--- wfos_ics/wfos-grxassembly/src/main/scala/wfos/grxassembly/GrxassemblyHandlers.scala
+++ wfos_ics/wfos-grxassembly/src/main/scala/wfos/grxassembly/GrxassemblyHandlers.scala
@@ -1,17 +1,28 @@
 package wfos.grxassembly
 
+import akka.actor.typed.{ActorRef, Scheduler}
 import akka.actor.typed.scaladsl.ActorContext
+import akka.util.Timeout
+import com.typesafe.config.ConfigFactory
 import csw.command.client.messages.TopLevelActorMessage
 import csw.framework.models.CswContext
 import csw.framework.scaladsl.ComponentHandlers
 import csw.location.api.models.TrackingEvent
+import csw.params.commands.CommandIssue.UnsupportedCommandIssue
 import csw.params.commands.CommandResponse._
-import csw.params.commands.ControlCommand
-import csw.time.core.models.UTCTime
+import csw.params.commands.{CommandName, ControlCommand, Setup}
 import csw.params.core.models.Id
-import csw.prefix.models.{Prefix, Subsystem}
+import csw.prefix.models.Prefix
+import csw.time.core.models.UTCTime
+import wfos.grxassembly.models.AssemblyConfiguration
+import wfos.grxassembly.models.GripperCommand
+import wfos.grxassembly.models.GripperCommand.IsValidMove
+import wfos.grxassembly.models.GripperPosition
 
-import scala.concurrent.ExecutionContextExecutor
+import wfos.grxassembly.command.{SelectCommand, BlueSelectCommand}
+
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
+import scala.concurrent.{Await, ExecutionContext}
 
 /**
  * Domain specific logic should be written in below handlers.
@@ -24,9 +35,15 @@ import scala.concurrent.ExecutionContextExecutor
 class GrxassemblyHandlers(ctx: ActorContext[TopLevelActorMessage], cswCtx: CswContext) extends ComponentHandlers(ctx, cswCtx) {
 
   import cswCtx._
-  implicit val ec: ExecutionContextExecutor = ctx.executionContext
-  private val log                           = loggerFactory.getLogger
-  private val prefix: Prefix                = cswCtx.componentInfo.prefix
+  implicit val a: Scheduler = ctx.system.scheduler
+
+  implicit val ec: ExecutionContext = ctx.executionContext
+  private val log                   = loggerFactory.getLogger
+  private val prefix: Prefix        = cswCtx.componentInfo.prefix
+
+  //  val initialPosition: GripperPosition
+
+  val selectCommand: SelectCommand = BlueSelectCommand
 
   override def initialize(): Unit = {
     log.info(s"Assembly: $prefix initialize")
@@ -34,7 +51,36 @@ class GrxassemblyHandlers(ctx: ActorContext[TopLevelActorMessage], cswCtx: CswCo
 
   override def onLocationTrackingEvent(trackingEvent: TrackingEvent): Unit = {}
 
-  override def validateCommand(runId: Id, controlCommand: ControlCommand): ValidateCommandResponse = Accepted(runId)
+  override def validateCommand(runId: Id, controlCommand: ControlCommand): ValidateCommandResponse = {
+    val timeout: FiniteDuration = 1.seconds
+    implicit val value: Timeout = Timeout(timeout)
+
+    val validateParamsRes = controlCommand match {
+      case cmd: Setup => validateSetupParams(runId, cmd)
+      case observe    => Invalid(runId, UnsupportedCommandIssue(s"$observe command not supported."))
+    }
+    validateParamsRes
+
+//    validateParamsRes match {
+//      case _: Accepted => Await.result(filterActor ? (IsValidMove(runId, _)), timeout)
+//      case invalidRes  => invalidRes
+//    }
+  }
+  private def validateSelectParams(runId: Id, setup: Setup): ValidateCommandResponse =
+    selectCommand.Validate(setup) match {
+      case Right(_) => Accepted(runId)
+      case Left(commandIssue: UnsupportedCommandIssue) =>
+        log.error(s"grx Assembly: Failed to validate, reason ${commandIssue.reason}")
+        Invalid(runId, UnsupportedCommandIssue(s"Validation Failed"))
+    }
+
+  private def validateSetupParams(runId: Id, setup: Setup): ValidateCommandResponse = setup.commandName match {
+    case selectCommand.Name => validateSelectParams(runId, setup)
+    case CommandName(name) =>
+      val errMsg = s"grx Assembly: Setup command: $name not supported."
+      log.error(errMsg)
+      Invalid(runId, UnsupportedCommandIssue(errMsg))
+  }
 
   override def onSubmit(runId: Id, controlCommand: ControlCommand): SubmitResponse = Completed(runId)
 
